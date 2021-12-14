78e093012da1babc713eee3fd4ba3226d2603b60
Naomi Williams <S11545100@students.duvalschools.org>
2021-12-13 21:44:28 -0500

    v0.1
    Imported modules for PyGame, System, and Time.
    
    Initialized the PyGame Module.
    
    Setup Window dimensions.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with PyGame, Naomi Williams, 12/12/21, 9:21PM, v0.0
+# Simple Animation with PyGame, Naomi Williams, 12/12/21, 9:25PM, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+
+# Setup the Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption ('Animation Example! ')
