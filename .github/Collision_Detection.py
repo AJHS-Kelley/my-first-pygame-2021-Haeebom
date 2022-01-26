4608b18b34fbe5dfd97b3f8527b0e5540e5e0061
Haeebom <S11545100@students.duvalschools.org>
2022-01-26 15:54:51 -0500

    v0.6
    Started the game loop.
    
     Created the code to check for arrowkeys/WASD movement.

--- .github/Collision_Detection.py
+++ .github/Collision_Detection.py
@@ -1,5 +1,6 @@
-# Pygame Collision Detection Practice, Naomi Williams, Januaray 26, 2022, 12:01am, v0.5
+# Pygame Collision Detection Practice, Naomi Williams, Januaray 26, 2022, 3:01am, v0.6
 
+from operator import truediv
 import pygame, sys, random
 from pygame.locals import *
 
@@ -36,3 +37,28 @@ moveDown = False
 
 MOVESPEED = 6
 
+# Run the game loop.
+while True:
+ # Check for events.
+  for event in pygame.event.get():
+            if event.type == QUIT :
+               pygame.quit()
+               sys.exit()
+            if event.type == KEYDOWN:
+                # CHange the keyboard variables.
+                if event.key == K_LEFT or event.key == K_a:
+                    moveRight = False
+                    moveLeft = True
+                if event.key == K_RIGHT or event.key == K_d:
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == K_w:
+                    moveDown = False
+                    moveUp = True
+                if event.key == K_DOWN or event.key == K_s:
+                   moveUp = False
+                   moveDown = True
+                   
+
+
+             
